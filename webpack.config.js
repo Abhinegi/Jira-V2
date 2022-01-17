7e1494c314f78855ff474fd02bffa25423bab34a
Abhishek Negi <negi.abhishek989@gmail.com>
2022-01-17 21:39:19 +0530

    Added env variable access in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,29 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 
-const webpackConfig = () => ({
-    entry:"./src/index.js",
-    output:{
+const webpackConfig = () => {
+    //Getting command line args from npm command in package.json
+    console.log(process.argv);
+    return {
+        entry:"./src/index.js",
+        output:{
         path : path.resolve(__dirname,'/dist'),
-        filename : 'bundle.js'
-    },
-    module:{
-        rules : [
-            {
-                test:/\.(js|jsx)$/,
-                exclude:/node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                  }
-            }
-        ]
-    },
-    plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })]
-});
+            filename : 'bundle.js'
+        },
+        module:{
+            rules : [
+                {
+                    test:/\.(js|jsx)$/,
+                    exclude:/node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                }
+            ]
+        },
+        plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })]
+    }
+};
 
 
 module.exports = webpackConfig();
